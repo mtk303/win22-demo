c7da0b8bf6f95f36b88c43e9be2b3e462db9d5d0
MayThinKyi303 <maythinkyi2003@gmail.com>
2024-03-12 20:48:54 +0630

    update footer logo

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -13,21 +13,28 @@ import menu from '../../assets/img/menu.png'
 
 
 import { NavLink } from 'react-router-dom'
+import { IoHomeOutline } from "react-icons/io5";
+import { MdOutlineAccountBalanceWallet } from "react-icons/md";
+import { CiGrid41 } from "react-icons/ci";
+import { FiGift } from "react-icons/fi";
+import { FaViber } from "react-icons/fa";
 
 const Footer = () => {
 
   const footerAfterLogin = [
-    { id: 1, img: home, link: '/', title: "အိမ်" },
-    { id: 2, img: deposit, link: '/wallet', title: "ပိုက်ဆံအိတ်" },
-    { id: 3, img: menu, link: '/', title: "", isMiddle: true },
-    { id: 4, img: promotion2, link: '/promotion', title: "ပရိုမိုးရှင်း" },
-    { id: 5, img: viber, link: '/', title: "viber" },
+    { id: 1, icon: <IoHomeOutline size={26} />, link: '/', title: "အိမ်" },
+    { id: 2, icon: <MdOutlineAccountBalanceWallet size={26} />, link: '/wallet', title: "ပိုက်ဆံအိတ်" },
+    // { id: 3, icon: <CiGrid41 size={26} />, link: '/', title: "", isMiddle: true },
+    { id: 4, icon: <FiGift size={26} />, link: '/promotion', title: "ပရိုမိုးရှင်း" },
+    { id: 5, icon: <FaViber size={26} />, link: '/', title: "viber" },
   ];
   return (
     <div className='slotFooter px-2 px-sm-5 py-2 rounded-top-5 d-flex justify-content-between align-items-center d-lg-none'>
       {footerAfterLogin.map(item => {
-        return <NavLink className={`${item.isMiddle ? 'middleIcon' : ' '} text-decoration-none text-center text-light `} to={item.link}>
-          <img src={item.img} />
+        return <NavLink className={`
+       
+        text-decoration-none text-center text-light `} to={item.link}>
+          {item.icon}
           <p>{item.title}</p>
         </NavLink>
       })}
